0f0dc01a0e0ff9d4506ca8c80b681ed84ee5ad99
gosia <gosgie02@wp.pl>
2024-10-21 22:14:02 +0200

    lab2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,50 @@
 import {data} from './lab1/module-data';
-import CarProfile from './lab1/carProfile';
+import CarProfile from './lab2/components/carProfile';
 import './App.css'
 
+// function App() {
+//   return (
+    // <div className="App">
+    //   {data.map((car, index) => (
+    //     <CarProfile key={index} {...car} />
+    //   ))}
+    // </div>
+    
 function App() {
+  const menuItems = [
+    {
+      id: 1,
+      label: 'Home',
+      url: '/',
+      urlPattern: '/',
+      element: <Home />,
+    },
+    {
+      id: 2,
+      label: 'About',
+      url: '/about',
+      urlPattern: '/about',
+      element: <About />,
+    },
+    {
+      id: 3,
+      label: 'Contact',
+      url: '/contact',
+      urlPattern: '/contact',
+      element: <Contact />,
+    },
+  ];
+
   return (
-    <div className="App">
-      {data.map((car, index) => (
-        <CarProfile key={index} {...car} />
-      ))}
-    </div>
+    <Router>
+      <RootLayout>
+        <Routes>
+          {menuItems.map(item => (
+            <Route key={item.id} path={item.urlPattern} element={item.element} />
+          ))}
+        </Routes>
+      </RootLayout>
+    </Router>
   );
 }
 
---
+++ vite-project/src/lab2/layouts/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '10px', background: '#f1f1f1' }}>
+      <p>
+        <img src="path-to-logo.png" alt="University Logo" style={{ height: '50px' }} />
+      </p>
+      <p>Author: Jan Kowalski</p>
+      <p>Email: jan.kowalski@example.com</p>
+    </footer>
+  );
+}
+
+export default Footer;
